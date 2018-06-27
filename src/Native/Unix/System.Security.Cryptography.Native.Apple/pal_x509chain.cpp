fd74330311aac54ee22a8614fa43ca9a19a5ccb2
Maryam Ariyan <maryam.ariyan@microsoft.com>
2018-06-27 12:04:11 -0700

    Fixes compile on Mojave and test failure

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.cpp
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.cpp
@@ -165,7 +165,9 @@ static void MergeStatusCodes(CFTypeRef key, CFTypeRef value, void* context)
         *pStatus |= PAL_X509ChainUntrustedRoot;
     else if (CFEqual(keyString, CFSTR("BasicConstraints")))
         *pStatus |= PAL_X509ChainInvalidBasicConstraints;
-    else if (CFEqual(keyString, CFSTR("UsageConstraints")))
+    else if (CFEqual(keyString, CFSTR("UsageConstraints")) || CFEqual(keyString, CFSTR("BlackListedLeaf")) ||
+             CFEqual(keyString, CFSTR("GrayListedLeaf")) || CFEqual(keyString, CFSTR("GrayListedKey")) ||
+             CFEqual(keyString, CFSTR("BlackListedKey")))
         *pStatus |= PAL_X509ChainExplicitDistrust;
     else if (CFEqual(keyString, CFSTR("RevocationResponseRequired")))
         *pStatus |= PAL_X509ChainRevocationStatusUnknown;
