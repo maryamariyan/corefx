1e79436fce7ba2d7bf29564de350ffcd99f4366a
Maryam Ariyan <maryam.ariyan@microsoft.com>
2018-12-20 11:22:08 -0800

    Uncommenting EventLogReaderTests

--- src/System.Diagnostics.EventLog/tests/System/Diagnostics/Reader/EventLogReaderTests.cs
+++ src/System.Diagnostics.EventLog/tests/System/Diagnostics/Reader/EventLogReaderTests.cs
@@ -9,78 +9,77 @@ namespace System.Diagnostics.Tests
 {
     public class EventLogReaderTests
     {
-        // [ConditionalTheory(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
-        // [InlineData("Application", false)]
-        // [InlineData("Application", true)]
-        // public void ReadEvent(string logName, bool useQuery)
-        // {
-        //     var eventLog =
-        //         useQuery
-        //         ? new EventLogReader(
-        //              new EventLogQuery(logName, PathType.LogName) { ReverseDirection = true })
-        //         : new EventLogReader(logName);
+        [ConditionalTheory(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        [InlineData("Application", false)]
+        [InlineData("Application", true)]
+        public void ReadEvent(string logName, bool useQuery)
+        {
+            var eventLog =
+                useQuery
+                ? new EventLogReader(
+                     new EventLogQuery(logName, PathType.LogName) { ReverseDirection = true })
+                : new EventLogReader(logName);
 
-        //     using (eventLog)
-        //     {
-        //         var record = eventLog.ReadEvent();
-        //         Assert.NotNull(record);
-        //         Assert.Equal(logName, record.LogName);
-        //     }
-        // }
+            using (eventLog)
+            {
+                var record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal(logName, record.LogName);
+            }
+        }
 
-        // [ConditionalTheory(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
-        // [InlineData("Microsoft-Windows-PowerShell/Operational", false)]
-        // [InlineData("Microsoft-Windows-PowerShell/Operational", true)]
-        // public void ReadEventPsh(string logName, bool useQuery)
-        // {
-        //     if (PlatformDetection.IsWindows7) // Null events in PowerShell log
-        //         return;
+        [ConditionalTheory(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        [InlineData("Microsoft-Windows-PowerShell/Operational", false)]
+        [InlineData("Microsoft-Windows-PowerShell/Operational", true)]
+        public void ReadEventPsh(string logName, bool useQuery)
+        {
+            if (PlatformDetection.IsWindows7) // Null events in PowerShell log
+                return;
 
-        //     var eventLog =
-        //         useQuery
-        //         ? new EventLogReader(
-        //              new EventLogQuery(logName, PathType.LogName) { ReverseDirection = true })
-        //         : new EventLogReader(logName);
+            var eventLog =
+                useQuery
+                ? new EventLogReader(
+                     new EventLogQuery(logName, PathType.LogName) { ReverseDirection = true })
+                : new EventLogReader(logName);
 
-        //     using (eventLog)
-        //     {
-        //         EventRecord record = eventLog.ReadEvent();
-        //         Assert.NotNull(record);
-        //         Assert.Equal(logName, record.LogName);
-        //     }
-        // }
+            using (eventLog)
+            {
+                EventRecord record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal(logName, record.LogName);
+            }
+        }
 
-        // [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
-        // public void Seek()
-        // {
-        //     using (var eventLog = new EventLogReader("Application"))
-        //     {
-        //         EventRecord record = eventLog.ReadEvent();
-        //         eventLog.Seek(record.Bookmark);
-        //         eventLog.Seek(IO.SeekOrigin.Begin, 0);
-        //         eventLog.Seek(IO.SeekOrigin.Current, 0);
-        //         eventLog.Seek(IO.SeekOrigin.End, 0);
-        //     }
-        // }
+        [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        public void Seek()
+        {
+            using (var eventLog = new EventLogReader("Application"))
+            {
+                EventRecord record = eventLog.ReadEvent();
+                eventLog.Seek(record.Bookmark);
+                eventLog.Seek(IO.SeekOrigin.Begin, 0);
+                eventLog.Seek(IO.SeekOrigin.Current, 0);
+                eventLog.Seek(IO.SeekOrigin.End, 0);
+            }
+        }
 
+        [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        public void CancelReading()
+        {
+            using (var eventLog = new EventLogReader("Application"))
+            {
+                eventLog.CancelReading();
+            }
+        }
 
-        // [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
-        // public void CancelReading()
-        // {
-        //     using (var eventLog = new EventLogReader("Application"))
-        //     {
-        //         eventLog.CancelReading();
-        //     }
-        // }
-
-        // [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
-        // public void LogStatus()
-        // {
-        //     using (var eventLog = new EventLogReader("Application"))
-        //     {
-        //         Assert.NotEmpty(eventLog.LogStatus);
-        //     }
-        // }
+        [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        public void LogStatus()
+        {
+            using (var eventLog = new EventLogReader("Application"))
+            {
+                Assert.NotEmpty(eventLog.LogStatus);
+            }
+        }
     }
 }
 
